5f16066eb8aff81995fb808c36733831f67a9227
MsMaddyMac <maddy.mcintosh@divvypay.com>
2022-05-10 17:12:21 -0400

    completes basic level of odyssey

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,39 @@
-//TODO
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./schema')
+
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(6)],
+  }),
+  Track: () => ({
+    id: () => 'track_01',
+    title: () => 'Astro Kitty, Space Explorer',
+    author: () => {
+      return {
+        name: 'Grumpy Cat',
+        photo:
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+      }
+    },
+    thumbnail: () =>
+      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+}
+// create instance of ApolloServer class and pass it our typeDefs
+// mocks: true enables basic mocked data and will instruct Apollo Server
+// to populate every queried schema field with a placeholder value
+const server = new ApolloServer({
+  typeDefs,
+  mocks,
+})
+
+// start up ApolloServer
+server.listen({ port: 7001 }).then(() => {
+  console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 7001
+      📭  Query at https://studio.apollographql.com/dev
+    `)
+})
